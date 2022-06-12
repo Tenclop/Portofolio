403273e8685658cf8efaf36ba4789a3049b8922d
Sihlanic <alin.sihlanic@yahoo.com>
2022-06-12 12:16:28 +0300

    gsap mouse follower animation

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -15,6 +15,17 @@ const animations = {
 
     gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
+    /* gsap mouse follower */
+    gsap.set(".ball", { xPercent: -50, yPercent: -50 });
+
+    let xTo = gsap.quickTo(".ball", "x", { duration: 0.6, ease: "power3" }),
+      yTo = gsap.quickTo(".ball", "y", { duration: 0.6, ease: "power3" });
+
+    window.addEventListener("mousemove", (e) => {
+      xTo(e.clientX);
+      yTo(e.clientY);
+    });
+
     /* Main navigation */
     let panelsSection = sections,
       panelsContainer = document.querySelector(".container"),
